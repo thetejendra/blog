2fe07d12265879e9fd4cf96d6afa11157c644b23
thetejendra <98267464+thetejendra@users.noreply.github.com>
2022-01-26 23:32:35 +0530

    placement

---
+++ placementFormCheck.php
@@ -0,0 +1,39 @@
+<?php
+// include "../db_conn.php";
+
+$sname= "localhost";
+$uname= "root";
+$password = "";
+
+$db_name = "quora";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
+
+if(isset($_POST['publish'])){
+    $name = $_POST['name'];
+    $company = $_POST['company'];
+    $role = $_POST['role'];
+    $name = mysqli_real_escape_string($conn, $name);
+    $company = mysqli_real_escape_string($conn, $company);
+    $role = mysqli_real_escape_string($conn, $role);
+    $name= htmlentities($name);
+    $company= htmlentities($company);
+    $role= htmlentities($role);
+
+        $sql= "insert into placement(name, company, role) values('$name', '$company', '$role')";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+            header("Location: placement.php");
+            $_SESSION['message']=" Successfully Registered";
+            $_SESSION['name']=$name;
+        }else{
+            header("Location: placement.php");
+            $_SESSION['message']=" Sorry Something Went Wrong !! Please Signup Again ...";
+        }
+
+
+}
